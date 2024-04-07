d6cd5cf6d17700f3c239ad34176ddba5d9812ce5
Majinka10 <camachopinto_@hotmail.com>
2024-04-07 17:28:55 -0500

    Agregando el componente como pagina inicial

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,13 +1,17 @@
 import { Routes, RouterModule } from '@angular/router';
 
 import { IngresoComponent } from './ingreso/ingreso.component';
-import { RegistroComponent } from './registro/registro.component';
 import { PrincipalComponent } from './principal/principal.component';
 import { loginGuard } from './login.guard';
+import { HomeComponent } from './home/home.component';
+
 
 export const routes: Routes = [
     { path: 'ingreso', component: IngresoComponent },
-    { path: 'registro', component: RegistroComponent },
     { path: 'principal', component: PrincipalComponent , canActivate: [loginGuard]},
-    { path: '', redirectTo: '/ingreso', pathMatch: 'full' }
+    {
+        path: '',
+        component: HomeComponent,
+        title: 'Finanzify'
+      }
 ];
